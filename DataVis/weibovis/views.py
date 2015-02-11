576d83d7d896a549c1b5b5170367eb0e9eabf419
ronaldhan <ronald.han7@gmail.com>
2015-02-11 11:06:38 +0800

    test of app

--- DataVis/weibovis/views.py
+++ DataVis/weibovis/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("hello django \n <a href='/weibovis/about'>about</a>")
+
+
+def about(request):
+    return HttpResponse("this is about page \n <a href='/weibovis/'>index</a>")
