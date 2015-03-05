47fbd0536bce2feb8dc1f8e3a1dab620361970bd
ronaldhan <ronald.han7@gmail.com>
2015-03-05 16:32:07 +0800

    make the host send json result to front and display the result

--- DataVis/weibovis/urls.py
+++ DataVis/weibovis/urls.py
@@ -5,4 +5,5 @@ from weibovis import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^about/$', views.about, name='about'),
-                       url(r'^mapdata/$', views.mapdata, name='mapdata'))
+                       url(r'^mapdata/$', views.mapdata, name='mapdata'),
+                       url(r'^getdata/$', views.getdata, name='getdata'))
--- DataVis/weibovis/views.py
+++ DataVis/weibovis/views.py
@@ -1,5 +1,7 @@
+# -*- coding:utf-8 -*-
+import random
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader, Context
 
 
@@ -14,3 +16,45 @@ def about(request):
 
 def mapdata(request):
     return render_to_response('weibovis/index.html')
+
+
+def getdata(request):
+    # deal with the request from front, now it is map data
+    querydict = request.GET
+    kind = querydict['kind']
+
+    if kind == 'map':
+        # construct map data
+        result = get_map_data()
+        print JsonResponse(result, safe=False)
+    return JsonResponse(result, safe=False)
+
+
+def get_map_data():
+    series_data = [
+        {'name': 1, 'value': 10, 'geoCoord': [116.090316745, 39.8066538017]},
+        {'name': 2, 'value': 10, 'geoCoord': [116.14783293, 39.8037860467]},
+        {'name': 3, 'value': 10, 'geoCoord': [116.168974437, 39.8040199854]},
+        {'name': 4, 'value': 10, 'geoCoord': [116.154235095, 39.8162342218]},
+        {'name': 5, 'value': 10, 'geoCoord': [116.180089174, 39.8079383742]},
+        {'name': 6, 'value': 10, 'geoCoord': [116.148811202, 39.8080363974]}
+    ]
+    result = dict()
+    result['series'] = series_data
+    return result
+
+
+def make_data(places):
+    data = []
+    plen = len(places)
+    for i in range(plen):
+        geocoord = places[i]['geoCoord']
+        data.append({
+            'name': places[i]['name'],
+            'value': 10,
+            'geoCoord': [
+                geocoord[0] + random.random() * 5 * -1,
+                geocoord[1] + random.random() * 3 * -1
+            ]
+        })
+    return data
