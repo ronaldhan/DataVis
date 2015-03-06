6f767e4064a724dd6f3d46fc509798949b40fe2c
ronaldhan <ronald.han7@gmail.com>
2015-03-06 11:41:41 +0800

    add new model StatsData to store stats data of weibo in May

--- DataVis/weibovis/models.py
+++ DataVis/weibovis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+# weibo data in one month
+class StatsData(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    pntcnt = models.IntegerField()
+
+    def __unicode__(self):
+        result = dict()
+        result['name'] = self.id
+        result['value'] = self.pntcnt
+        result['geoCoord'] = [self.x, self.y]
+        return result
